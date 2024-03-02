e25ae23f3cbabfadc1093f866cb16a41103b52f1
Karyqqe <kirillbeliaev8@gmail.com>
2024-03-02 12:58:23 +0300

    correction

--- lib/main.dart
+++ lib/main.dart
@@ -16,108 +16,109 @@ class MyApp extends StatelessWidget {
         body: Align(
 
 
-          child: Row(
-            children: [
-              Expanded(
-                flex: 1,
-                child: Column(
-                  children: [
-                    Expanded(flex: 4, child:
 
-                    Container(
-                      alignment: Alignment.center,
-                      child: Text('11', style: TextStyle(fontSize: 36),),
-                      width: 400,
-                      height: 450,
-                      color: Colors.blue,
-                      margin: EdgeInsets.only(bottom: 8, right: 8, top: 8, left: 8),
+          child: Container(
+            width: double.infinity,
+            height: double.infinity,
 
-                    )),
-                    Expanded(flex: 1, child:
 
-                    Container(
-                      alignment: Alignment.center,
-                      child: Text('12', style: TextStyle(fontSize: 36),),
-                      width: 400,
-                      height: 450,
-                      color: Colors.deepOrange,
+            child: Row(
+              children: [
+                Expanded(
+                  flex: 1,
+                  child: Column(
+                    children: [
+                      Expanded(flex: 4, child:
+
+                      Container(
+
+                        alignment: Alignment.center,
+                        child: Text('11', style: TextStyle(fontSize: 36),),
+
+                        color: Colors.blue,
+                        margin: EdgeInsets.only(bottom: 8, right: 8, top: 8, left: 8),
+
+                      )),
+                      Expanded(flex: 1, child:
+
+                      Container(
+                        alignment: Alignment.center,
+                        child: Text('12', style: TextStyle(fontSize: 36),),
+
+                        color: Colors.deepOrange,
                         margin: EdgeInsets.only(bottom: 8, right: 8, left: 8),
 
-                    )),
-                    Expanded(flex: 1, child:
+                      )),
+                      Expanded(flex: 1, child:
 
-                    Container(
-                      alignment: Alignment.center,
-                      child: Text('13', style: TextStyle(fontSize: 36),),
-                      width: 400,
-                      height: 450,
-                      color: Colors.black45,
+                      Container(
+                        alignment: Alignment.center,
+                        child: Text('13', style: TextStyle(fontSize: 36),),
+
+                        color: Colors.black45,
                         margin: EdgeInsets.only(bottom: 8, right: 8, left: 8),
 
-                    )),
-                    Expanded(flex: 4, child:
+                      )),
+                      Expanded(flex: 4, child:
 
-                    Container(
-                      alignment: Alignment.center,
-                      child: Text('14', style: TextStyle(fontSize: 36),),
-                      width: 400,
-                      height: 450,
-                      color: Colors.brown,
+                      Container(
+                        alignment: Alignment.center,
+                        child: Text('14', style: TextStyle(fontSize: 36),),
+
+                        color: Colors.brown,
                         margin: EdgeInsets.only(bottom: 8, right: 8, left: 8),
 
-                    ),),],),),
-                  Expanded(
-                      flex: 1,
-                      child: Column(
-                          children: [
-                          Expanded(flex: 4, child:
+                      ),),],),),
+                Expanded(
+                    flex: 1,
+                    child: Column(
+                      children: [
+                        Expanded(flex: 4, child:
 
-                            Container(
-                              alignment: Alignment.center,
-                              child: Text('21', style: TextStyle(fontSize: 36),),
-                          width: 400,
-                          height: 450,
+                        Container(
+                          alignment: Alignment.center,
+                          child: Text('21', style: TextStyle(fontSize: 36),),
+
                           color: Colors.amberAccent,
                           margin: EdgeInsets.only(bottom: 8, top: 8, right: 8),
 
                         )),
                         Expanded(flex: 1, child:
 
-                         Container(
-                           alignment: Alignment.center,
-                           child: Text('22', style: TextStyle(fontSize: 36),),
-                          width: 400,
-                          height: 450,
+                        Container(
+                          alignment: Alignment.center,
+                          child: Text('22', style: TextStyle(fontSize: 36),),
+
                           color: Colors.cyan,
                           margin: EdgeInsets.only(bottom: 8, right: 8),
 
                         )),
                         Expanded(flex: 1, child:
 
-                          Container(
-                            alignment: Alignment.center,
-                            child: Text('23', style: TextStyle(fontSize: 36),),
-                          width: 400,
-                          height: 450,
+                        Container(
+                          alignment: Alignment.center,
+                          child: Text('23', style: TextStyle(fontSize: 36),),
+
                           color: Colors.blueGrey,
                           margin: EdgeInsets.only(bottom: 8, right: 8),
 
                         )),
                         Expanded(flex: 4, child:
 
-                          Container(
-                            alignment: Alignment.center,
-                            child: Text('24', style: TextStyle(fontSize: 36),),
-                          width: 400,
-                          height: 450,
+                        Container(
+                          alignment: Alignment.center,
+                          child: Text('24', style: TextStyle(fontSize: 36),),
+
                           color: Colors.indigo,
                           margin: EdgeInsets.only(bottom: 8, right: 8),
-                      ),),],)
+                        ),),],)
 
 
                 ),
 
-          ],
+              ],
+
+            ),
 
 
 
